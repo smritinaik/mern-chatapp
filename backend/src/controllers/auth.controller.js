45cb30ad6091090edeeeb7a132e321b3987b6b54
smritinaik <148971067+smritinaik@users.noreply.github.com>
2024-12-28 17:18:23 +0530

    data base setup
    data base setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signup = (req , res) => {
+    res.send("signin route");
+}
+
+export const login = (req , res) => {
+    res.send("signin route");
+}
+
+export const logout = (req , res) => {
+    res.send("signin route");
+}
+
+// RhhkFkmK1DQbMewP
